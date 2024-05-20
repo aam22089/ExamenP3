d38bdecdb20650bce203a7cc23a1d1405eb1044c
mar <117465108+marm0r@users.noreply.github.com>
2024-05-20 13:49:38 -0600

    bolita

--- main.cpp
+++ main.cpp
@@ -1,38 +1,40 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "bolita.hpp"
 using namespace std;
 using namespace sf;
-int main()
-{
+
+
+
+int main() {
     RenderWindow window(VideoMode(450, 400), "SFML works!");
     window.setFramerateLimit(60);
-    Texture yoshitexture;
-    if(!yoshitexture.loadFromFile("Yoshi.png"))
-    {
-cout << "Error al cargar imagen" << endl;
-    }
-    yoshitexture.setRepeated(true);
 
-    Sprite yoshi;
-    yoshi.setTexture(yoshitexture);
-    yoshi.setTextureRect(IntRect(0,0,700,700));
-    int opacidad=0;
+    Bolita bolita(200.f, 200.f, 2.f);
+    CircleShape shape(10.f);
+    shape.setFillColor(Color::Red);
 
-    while (window.isOpen())
-    {
+    while (window.isOpen()) {
         Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == Event::Closed)
                 window.close();
         }
 
+        if (Keyboard::isKeyPressed(Keyboard::Left))
+            bolita.mover(-1.f, 0.f);
+        if (Keyboard::isKeyPressed(Keyboard::Right))
+            bolita.mover(1.f, 0.f);
+        if (Keyboard::isKeyPressed(Keyboard::Up))
+            bolita.mover(0.f, -1.f);
+        if (Keyboard::isKeyPressed(Keyboard::Down))
+            bolita.mover(0.f, 1.f);
+
+        shape.setPosition(bolita.getX(), bolita.getY());
+
         window.clear();
-        yoshi.rotate(5);
-        window.draw(yoshi);
-        yoshi.setColor(Color(rand()%256,rand()%256,rand()%256,255));
+        window.draw(shape);
         window.display();
-        opacidad++;
     }
 
     return 0;
---
+++ mapa.hpp
@@ -0,0 +1,9 @@
+#ifndef mapa_hpp
+#define mapa_hpp
+
+class Mapa
+{
+
+};
+
+#endif
