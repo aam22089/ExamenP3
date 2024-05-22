9488a4b8a3a7a16a579a9ed3e19ea92b298da44d
mar <117465108+marm0r@users.noreply.github.com>
2024-05-22 13:10:05 -0600

    a

--- main.cpp
+++ main.cpp
@@ -10,9 +10,6 @@ int main() {
     RenderWindow window(VideoMode(450, 400), "SFML works!");
     window.setFramerateLimit(60);
 
-    Bolita bolita(200.f, 200.f, 2.f);
-    CircleShape shape(10.f);
-    shape.setFillColor(Color::Red);
 
     while (window.isOpen()) {
         Event event;
@@ -21,19 +18,8 @@ int main() {
                 window.close();
         }
 
-        if (Keyboard::isKeyPressed(Keyboard::Left))
-            bolita.mover(-1.f, 0.f);
-        if (Keyboard::isKeyPressed(Keyboard::Right))
-            bolita.mover(1.f, 0.f);
-        if (Keyboard::isKeyPressed(Keyboard::Up))
-            bolita.mover(0.f, -1.f);
-        if (Keyboard::isKeyPressed(Keyboard::Down))
-            bolita.mover(0.f, 1.f);
-
-        shape.setPosition(bolita.getX(), bolita.getY());
 
         window.clear();
-        window.draw(shape);
         window.display();
     }
 
