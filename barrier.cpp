dcf184e850cec6038b486f3905cc79e46845ceff
aam22089 <22089@portalcedi.edu.mx>
2024-06-04 17:40:33 -0600

    prueba

--- barrier.cpp
+++ barrier.cpp
@@ -4,7 +4,7 @@ Barrier::Barrier(float b, float h, int x, int y, String direct)
 {
    if(!this->tpared.loadFromFile(direct))
    cout << "Error" << endl;
-   
+
    this->spared.setTexture(tpared);
     this->Hitbox.setSize(Vector2f(b,h));
     this->Hitbox.setPosition(x,y);
